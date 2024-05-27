94a3191ed31c7505def376397b9e7615f24645a4
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-05-27 12:15:06 +0100

    feat: Created the index file for the svg folder

---
+++ src/svg/index.js
@@ -0,0 +1,29 @@
+import CreditCardIcon from "./CreditCard";
+import EconometricsIcon from "./Econometrics";
+import HomeIcon from "./Home";
+import InvestmentIcon from "./Investment";
+import LoanIcon from "./Loan";
+import ServiceIcon from "./Service";
+import SettingIcon from "./Setting";
+import TransferIcon from "./Transfer";
+import UserIcon from "./User";
+import CoinIcon from "./Coin";
+import CardIcon from "./Card";
+import PaypalIcon from "./Paypal";
+import TelegramIcon from "./Telegram";
+
+export {
+  CreditCardIcon,
+  TelegramIcon,
+  CoinIcon,
+  CardIcon,
+  PaypalIcon,
+  HomeIcon,
+  InvestmentIcon,
+  EconometricsIcon,
+  LoanIcon,
+  ServiceIcon,
+  SettingIcon,
+  TransferIcon,
+  UserIcon
+};
