0bafd850f3cc2eed09061e664e1d318f79ad4f11
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-01 10:33:06 +0100

    fix: pass some props to the Root component

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import MainNavigation from "./../components/MainNavigation/MainNavigation";
 
 export default function Root() {
+  const [isNavClosed, setIsNavClosed] = useState(false);
   return (
-    <div className="flex w-full relative">
-      <MainNavigation />
-      <Outlet />
+    <div className="  w-full relative">
+      <MainNavigation
+        isNavClosed={isNavClosed}
+        onSetIsNavClosed={setIsNavClosed}
+      />
+      <Outlet context={[isNavClosed, setIsNavClosed]} />
     </div>
   );
 }
