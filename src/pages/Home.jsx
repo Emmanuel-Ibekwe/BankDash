c08f06d6e5d4f6280bbd9e14641cba56005a8101
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-01 10:32:33 +0100

    fix: pass some props to Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
+
 import Header from "../components/Header/Header";
-import DashboardBody from "../components/Overview/Overview";
+import Overview from "./../components/Overview/Overview";
 
 export default function Home() {
+  const [isNavClosed, setIsNavClosed] = useOutletContext();
   return (
-    <div className="lg:body-width-lg xl:body-width-xl absolute lg:left-[230px] xl:left-60">
-      <Header />
-      <DashboardBody />
+    <div className="w-full lg:body-width-lg xl:body-width-xl static lg:absolute lg:left-[230px] xl:left-60">
+      <Header isNavClosed={isNavClosed} onSetIsNavClosed={setIsNavClosed} />
+      <Overview />
     </div>
   );
 }
