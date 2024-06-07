2005cb4d84ce76ea2ec4e852cd85db11fb25b5d1
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-07 11:25:11 +0100

    feat: add the accounts route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "./pages/Root";
 import Home from "./pages/Home";
+import TransactionsPage from "./pages/TransactionsPage";
+import AccountsPage from "./pages/AccountsPage";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />}>
           <Route index element={<Home />} />
+          <Route path="transactions" element={<TransactionsPage />} />
+          <Route path="accounts" element={<AccountsPage />} />
         </Route>
       </Routes>
     </Router>
