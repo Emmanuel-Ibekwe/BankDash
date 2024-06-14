442b639720dd0aed02a126aa293f64aa9b7401e2
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-14 15:53:29 +0100

    feat: add the investments and the credit-cards routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "./pages/Root";
 import Home from "./pages/Home";
 import TransactionsPage from "./pages/TransactionsPage";
+import InvestmentsPage from "./pages/InvestmentsPage";
 import AccountsPage from "./pages/AccountsPage";
+import CreditCardsPage from "./pages/CreditCardsPage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="transactions" element={<TransactionsPage />} />
           <Route path="accounts" element={<AccountsPage />} />
+          <Route path="investments" element={<InvestmentsPage />} />
+          <Route path="credit-cards" element={<CreditCardsPage />} />
         </Route>
       </Routes>
     </Router>
