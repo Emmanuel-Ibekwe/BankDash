91685376b2487994cabd683015c551e5f400adce
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-14 16:10:24 +0100

    feat: create some icons

---
+++ src/svg/PieChartIcon.jsx
@@ -0,0 +1,67 @@
+export default function PieChartIcon({ className }) {
+  return (
+    <svg
+      width="38"
+      height="38"
+      viewBox="0 0 38 38"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g clip-path="url(#clip0_104_391)" filter="url(#filter0_d_104_391)">
+        <path
+          d="M19.8787 15.8783V0C11.3736 0.446115 4.4466 7.37308 4.00049 15.8783H19.8787ZM16.3633 13.2416H14.6055V11.4839H16.3633V13.2416ZM11.09 7.96842H12.8478V9.72616H11.09V7.96842ZM15.4844 7.60451L16.7272 8.84729L11.9689 13.6056L10.7261 12.3628L15.4844 7.60451Z"
+          fill="#FF82AC"
+        />
+        <path
+          d="M19.8787 17.636H9.36572C9.79403 23.2371 14.2776 27.7207 19.8787 28.149V17.636H19.8787Z"
+          fill="#FF82AC"
+        />
+        <path
+          d="M21.6362 3.5155C21.6362 3.70428 21.6362 30.2063 21.6362 29.9988C28.4956 29.5432 33.999 23.6423 33.999 16.6694C33.999 9.6959 28.4973 3.96953 21.6362 3.5155ZM26.9095 15.8783C28.3633 15.8783 29.5461 17.061 29.5461 18.5149C29.5461 19.6592 28.8091 20.6256 27.7883 20.9896V22.9092H26.0306V20.9896C25.0098 20.6256 24.2728 19.6592 24.2728 18.5149H26.0306C26.0306 18.9998 26.4246 19.3938 26.9095 19.3938C27.3944 19.3938 27.7883 18.9998 27.7883 18.5149C27.7883 18.03 27.3944 17.636 26.9095 17.636C25.4556 17.636 24.2728 16.4533 24.2728 14.9994C24.2728 13.8551 25.0098 12.8887 26.0306 12.5247V10.6051H27.7883V12.5247C28.8091 12.8887 29.5461 13.8551 29.5461 14.9994H27.7883C27.7883 14.5145 27.3944 14.1205 26.9095 14.1205C26.4246 14.1205 26.0306 14.5145 26.0306 14.9994C26.0306 15.4843 26.4246 15.8783 26.9095 15.8783Z"
+          fill="#FF82AC"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_104_391"
+          x="0"
+          y="0"
+          width="38"
+          height="38"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_104_391"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_104_391"
+            result="shape"
+          />
+        </filter>
+        <clipPath id="clip0_104_391">
+          <rect width="30" height="30" fill="white" transform="translate(4)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
