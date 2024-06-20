2914bcbc9631e916b9e3f29b4d585a6095ca6c97
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-20 13:40:08 +0100

    feat: add the loans, services and settings routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TransactionsPage from "./pages/TransactionsPage";
 import InvestmentsPage from "./pages/InvestmentsPage";
 import AccountsPage from "./pages/AccountsPage";
 import CreditCardsPage from "./pages/CreditCardsPage";
+import LoansPage from "./pages/LoansPage";
+import ServicesPage from "./pages/ServicesPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route path="accounts" element={<AccountsPage />} />
           <Route path="investments" element={<InvestmentsPage />} />
           <Route path="credit-cards" element={<CreditCardsPage />} />
+          <Route path="loans" element={<LoansPage />} />
+          <Route path="services" element={<ServicesPage />} />
+          <Route path="settings" element={<SettingsPage />} />
         </Route>
       </Routes>
     </Router>
