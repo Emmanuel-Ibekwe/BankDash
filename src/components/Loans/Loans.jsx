74fcf1eeb69679a357f56a4b3e59895ce67672be
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-27 14:37:55 +0100

    style: remove the mb-[1000px] class

--- src/components/Loans/Loans.jsx
+++ src/components/Loans/Loans.jsx
@@ -3,7 +3,7 @@ import ActiveLoansOverview from "./ActiveLoansOverview";
 
 export default function Loans() {
   return (
-    <div className=" w-full box-border pt-6 mt-[140px] mb-[1000px] lg:mt-0  z-50 lg:pt-[110px] xl:pt-[120px] px-5 lg:px-[2.5%] xl:px-[2%] :bg-background_1 pb-8">
+    <div className=" w-full box-border pt-6 mt-[140px] lg:mt-0  z-50 lg:pt-[110px] xl:pt-[120px] px-5 lg:px-[2.5%] xl:px-[2%] :bg-background_1 pb-8">
       <div className="w-full scrollbar1 overflow-x-scroll min-[1070px]:overflow-x-auto pb-2">
         <LoansSummary />
       </div>
