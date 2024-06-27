2abd88b70b5adec106cccb7390f9fc4aae963e6e
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-27 14:46:58 +0100

    style: update some of the classes to enable better responsiveness

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -8,8 +8,8 @@ export default function Settings() {
   const [option, setOption] = useState("edit profile");
 
   return (
-    <div className=" w-full box-border pt-6 mt-[140px] mb-[1000px] lg:mt-0  z-50 lg:pt-[110px] xl:pt-[120px] px-5 lg:px-[2.5%] xl:px-[2%] :bg-background_1 pb-8">
-      <div className="bg-white p-6 rounded-3xl w-full">
+    <div className=" w-full box-border pt-6 mt-[140px] mb-[120px] lg:mt-0  z-50 lg:pt-[110px] xl:pt-[120px] px-5 lg:px-[2.5%] xl:px-[2%] :bg-background_1 pb-8">
+      <div className="bg-white p-5 min-[600px]:p-6 rounded-2xl xl:rounded-3xl w-full">
         <SettingsOptions onSetOption={setOption} />
         {option === "edit profile" ? (
           <EditProfile />
