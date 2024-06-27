b2f60ef872f896c1f13153256e931d022eaa5142
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-06-27 14:39:12 +0100

    style:  update the breakpoints of the rounded-3xl in the 3 inner Loan divs

--- src/components/Loans/LoansSummary.jsx
+++ src/components/Loans/LoansSummary.jsx
@@ -8,7 +8,7 @@ export default function LoansSummary() {
   return (
     <div className="w-[992px] min-[833px]:w-full lg:w-[775px] min-[1070px]:w-full flex items-center justify-between">
       {/* Personal Laons */}
-      <div className="w-[23.18%] bg-white rounded-[20px] lg:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
+      <div className="w-[23.18%] bg-white rounded-[20px] xl:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
         <div className="w-[45px] xl:w-[70px] h-[45px] xl:h-[70px] rounded-full bg-[#E7EDFF] flex items-center justify-center">
           <UserIcon2 className="w-[18px] xl:w-fit h-[18px] xl:h-fit" />
         </div>
@@ -20,7 +20,7 @@ export default function LoansSummary() {
         </div>
       </div>
       {/*  Corporate Loans*/}
-      <div className="w-[23.18%] bg-white rounded-[20px] lg:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
+      <div className="w-[23.18%] bg-white rounded-[20px] xl:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
         <div className="w-[45px] xl:w-[70px] h-[45px] xl:h-[70px] rounded-full bg-[#FFF5D9] flex items-center justify-center">
           <BriefcaseIcon className="w-[18px] xl:w-fit h-[18px] xl:h-fit" />
         </div>
@@ -34,7 +34,7 @@ export default function LoansSummary() {
         </div>
       </div>
       {/*  Business Loans*/}
-      <div className="w-[23.18%] bg-white rounded-[20px] lg:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
+      <div className="w-[23.18%] bg-white rounded-[20px] xl:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
         <div className="w-[45px] xl:w-[70px] h-[45px] xl:h-[70px] rounded-full bg-[#FFE0EB] flex items-center justify-center">
           <GraphIcon className="w-[18px] xl:w-fit h-[18px] xl:h-fit" />
         </div>
@@ -46,7 +46,7 @@ export default function LoansSummary() {
         </div>
       </div>
       {/*  Custom Loans*/}
-      <div className="w-[23.18%] bg-white rounded-[20px] lg:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
+      <div className="w-[23.18%] bg-white rounded-[20px] xl:rounded-3xl flex items-center justify-start space-x-3 xl:justify-between px-8 min-[833px]:p-3.5 py-5 xl:p-5">
         <div className="w-[45px] xl:w-[70px] h-[45px] xl:h-[70px] rounded-full bg-[#DCFAF8] flex items-center justify-center">
           <ToolsIcon className="w-[18px] xl:w-fit h-[18px] xl:h-fit" />
         </div>
